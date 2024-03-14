dd85ac643e0ee48323755d653366e3188de2bd5d
anastasiyaaa0 <anastasiiiia98@gmail.com>
2024-03-14 18:32:55 +0200

    fix task5

--- js/task-5.js
+++ js/task-5.js
@@ -4,16 +4,12 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-function changeBackgroundColor() {
-  const body = document.querySelector('body');
-  const button = document.querySelector('button.change-color');
-  const colorSpan = document.querySelector('span.color');
+const body = document.querySelector('body');
+const button = document.querySelector('button.change-color');
+const colorSpan = document.querySelector('span.color');
 
-  button.addEventListener('click', event => {
-    const color = getRandomHexColor();
-    body.style.backgroundColor = color;
-    colorSpan.style.color = color;
-  });
-}
-
-changeBackgroundColor();
+button.addEventListener('click', event => {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorSpan.textContent = color;
+});
